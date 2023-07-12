0fffdff57d4b48bce92358e98fb2df3030f7f9f9
pritish <91678294+Pritish-25@users.noreply.github.com>
2023-07-12 09:47:25 +0530

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,5 +0,0 @@
-# C2Ladders
-
-solutions for some of the c2 ladder questions
-'make sure to do the pull request if you have a better method'
-Thanks!
---
+++ README.py
@@ -0,0 +1,5 @@
+'''          #C2Ladders
+
+   solutions for some of the c2 ladder questions
+   'make sure to do the pull request if you have a better method'
+   Thanks!'''
